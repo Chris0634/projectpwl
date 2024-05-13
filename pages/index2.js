57a1767700123dd4480f9eea767ff9df88909f67
Chris634 <2272016@maranatha.ac.id>
2024-05-13 12:38:01 +0700

    UPLOAD1

---
+++ pages/index2.js
@@ -0,0 +1,35 @@
+var http = require('http');
+var fileSys = require('fs');
+var url = require('url');
+
+var server = http.createServer((req, res)=>{
+    let q = url.parse(req.url, true);
+    let path =q.query;
+    let fileLocation;
+    switch (path.menu) {
+        case '/':
+            fileLocation = 'pages/home.html'
+            break;
+        case 'home':
+            fileLocation = 'pages/home.html'
+            break;
+        case 'kartukeluarga':
+            fileLocation = 'pages/kartukeluarga.html'
+            break;
+        case 'penduduk':
+            fileLocation = 'pages/penduduk.html'
+            break;
+        default:
+            fileLocation = 'pages/index.html'
+    }
+    fileSys.readFile(fileLocation,(err, data)=> {
+        if (err) {
+            res.writeHead(404, {'Content-type':'text/html'});
+            return res.end('404 Not Found');
+        }
+        res.writeHead(200, {'Content-type':'text/html'});
+        res.write(data);
+        return res.end();
+    });
+});
+server.listen(8000);
